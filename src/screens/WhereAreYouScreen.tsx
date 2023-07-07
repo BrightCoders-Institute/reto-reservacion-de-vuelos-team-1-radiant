3a9bde37d6aa08f2b64efbeefdc384de05f73a0b
OrlandoLeso <44180680+landillo@users.noreply.github.com>
2023-07-06 20:18:48 -0600

    feature picker whit flightInfo component
    Co-authored-by: Miguel Cobian Meraz <mcmmiguel@users.noreply.github.com>
    Co-authored-by: Iván Caetano <IvanCa27@users.noreply.github.com>
    Co-authored-by: Arnoldo Magaña <AMCernas@users.noreply.github.com>

--- src/screens/WhereAreYouScreen.tsx
+++ src/screens/WhereAreYouScreen.tsx
@@ -5,29 +5,62 @@ import { ButtonPrimary } from '../components/ButtonPrimary'
 import styles from '../appTheme/AppTheme';
 import FlightInfo from '../components/FlightInfo';
 import { SubTitle } from '../components/SubTitle';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
-export const WhereAreYouScreen = () => {
+export const WhereAreYouScreen = ({navigation} : any) => {
+
   const screenHeight = Dimensions.get('window').height;
   const flightInfoHeight = 0.3 * screenHeight;
   const rest = screenHeight - flightInfoHeight;
 
+  const [selectedFlight, setSelectedFlight] = useState(null);
+  const [isValid, setIsValid] = useState(false)
+
+
+  const handleSelectFlight = (flight: any) => { 
+    setSelectedFlight(flight);
+  }
+
+  const handleNext = () => {
+  };
+
+  
   return (
-    <View style={styles.whereAreYouContainer} >
-     
+    <View style={styles.whereAreYouContainer}>
       <View style={{flex: 0.3, maxHeight: flightInfoHeight}}>
-       <FlightInfo />
+        <View>
+
+        {selectedFlight ? <FlightInfo selectedFlight={selectedFlight} /> 
+          : null}
+
+        {selectedFlight ? <View style={styles.flightInfoIconContainer}>
+          <Icon name={'airplane'} size={25} color={'#2D7BDD'} />
+        </View>
+        : null}
+
+        <View>
+
+        </View>
+
+        </View>
       </View>
+
+      
 
       <View style={{flex: 0.7, maxHeight: rest}}>
-        <SubTitle text='Where are you now?'/>
-        <OriginDropDown/>
+        <SubTitle text="Where are you now?" /> 
+        <OriginDropDown onSelectFlight={handleSelectFlight} />
       </View>
 
-      <View style={{ marginBottom: '15%'}} >
-        <ButtonPrimary title="Next" />
-      </View>
+      {
+      /* <View style={{flex: 0.7, maxHeight: rest}}>
+        <SubTitle text="Where will you be flying to?" />
+      </View> */}
 
+      <View style={{marginBottom: '15%'}}>
+        <ButtonPrimary title="Next" onPress={() => navigation.navigate('FlightDestinationScreen')}/>
+      </View>
     </View>
   );
 };
